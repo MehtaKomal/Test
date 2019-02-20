884cc03483b170a9e5764d2f5feac4745e324a93
sks84 <komalmehta.8380@gmail.com>
2019-02-20 16:20:33 +0530

    Changes in settings file.
    Signed-off-by: sks84 <komalmehta.8380@gmail.com>

--- Sample/settings.py
+++ Sample/settings.py
@@ -152,8 +152,7 @@ EMAIL_HOST_PASSWORD = 'Admin@123'
 
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'komalmehta.8380@gmail.com'
-# EMAIL_HOST_PASSWORD = 'jpndvkdmm'
+
 
 # EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
 # # DEFAULT_FROM_EMAIL = 'admin@corhike.com'
